300cd5bc0926e1853a6f003c13b295ac49d5e049
George <george.j.london@gmail.com>
2023-10-13 22:09:32 -0700

    abstract likert choices and make easier to serialize to LLM friendly form

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
-from flask import Flask, render_template, jsonify, request, session
-from flask_cors import CORS
-from flask_wtf.csrf import CSRFProtect
-import requests, openai, os
-from dotenv.main import load_dotenv
-from langchain.llms import OpenAI
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationSummaryBufferMemory
-
-from flask import Flask, render_template, request, jsonify, session
-from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SubmitField, RadioField, SelectMultipleField, widgets
-from wtforms.validators import DataRequired
-from flask_wtf.csrf import CSRFProtect
+import json
 
 from environs import Env
+from flask import Flask, render_template, request, jsonify, session, make_response
+from flask_cors import CORS
+from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+from langchain.chains import ConversationChain
+from langchain.llms import OpenAI
+from langchain.memory import ConversationSummaryBufferMemory
+from wtforms import StringField, SelectField, SubmitField, RadioField, SelectMultipleField, widgets
+from wtforms.validators import DataRequired
 
 env = Env()
 # Read .env into os.environ
@@ -33,15 +28,25 @@ def chat():
     return render_template('index.html')
 
 
-@app.route('/form')
-def form():
-    return render_template('intake_form.html')
-
-
-# define a VoterInfo class to capture demographic and values information about the voter
-# and then a FlaskForm that uses VoterInfo as a model and captures those fields
-
 class VoterInfo:
+    """
+    define a VoterInfo class to capture demographic and values information about the voter
+    and then a FlaskForm that uses VoterInfo as a model and captures those fields
+
+    """
+    likert_choices = [
+        "housing",
+        "economy",
+        "environment",
+        "immigration",
+        "income_inequality",
+        "transportation",
+        "education",
+        "healthcare",
+        "public_safety",
+        "taxation",
+    ]
+
     def __init__(self):
         self.zip_code = None
         self.party_affiliation = None
@@ -57,8 +62,36 @@ class VoterInfo:
         self.public_safety = None
         self.taxation = None
 
+    @classmethod
+    def from_vals(cls, **kwargs):
+        """
+        alternative constructor that can take a dict of values and populate the fields
 
-import json
+        :param kwargs:
+        :return:
+        """
+        instance = cls()
+        for k, v in kwargs.items():
+            setattr(instance, k, v)
+        return instance
+
+    def for_llm(self):
+        """
+        output contents as a json-structured string we can feed to the llm.
+
+        for all the likert fields, convert the numeric value to the corresponding string value,
+        based on the LIKERT_CHOICES mapping.
+        :return: str
+        """
+        fields = self.__dict__
+        for likert_val in self.likert_choices:
+            numeric_val = fields[likert_val]
+            if numeric_val in LIKERT_LOOKUP:
+                fields[likert_val] = {
+                    'question': QUESTION_TEXT[likert_val],
+                    'response': LIKERT_LOOKUP[numeric_val]
+                }
+        return json.dumps(fields)
 
 
 class VoterInfoEncoder(json.JSONEncoder):
@@ -69,7 +102,7 @@ class VoterInfoEncoder(json.JSONEncoder):
 
 
 class VoterInfoDecoder(json.JSONDecoder):
-    def decode(self, json_str):
+    def decode(self, json_str, **kwargs):
         data = json.loads(json_str)
         voter_info = VoterInfo()
         voter_info.__dict__.update(data)
@@ -84,8 +117,22 @@ LIKERT_CHOICES = [
     ('5', 'Strongly Agree')
 ]
 
+# make a dict to look up the string for a given likert numeric value
+LIKERT_LOOKUP = dict(LIKERT_CHOICES)
 
-# create a form to
+QUESTION_TEXT = {
+    'housing': "Government intervention is necessary for affordable housing.",
+    'economy': "Job creation is more important than wealth redistribution in California.",
+    'environment': "Environmental protection is worth slowing economic growth in California.",
+    'immigration': "California's resources should primarily serve its citizens, not undocumented immigrants.",
+    'income_inequality': "Reducing income inequality is more important than promoting business growth in California.",
+    'transportation': "Public transportation is more important than private vehicle infrastructure in California.",
+    'education': "Public education funding is more important than tax cuts in California.",
+    'healthcare': "A single-payer healthcare system is preferable to private healthcare in California.",
+    'public_safety': "Community programs are more effective than traditional law enforcement in California.",
+    'taxation': "Progressive taxation is more beneficial than lower taxes for all in California."
+}
+
 
 class IntakeForm(FlaskForm):
     zip_code = StringField('Zip Code', validators=[DataRequired()])
@@ -108,38 +155,29 @@ class IntakeForm(FlaskForm):
                                            widget=widgets.ListWidget(prefix_label=False),
                                            validators=[DataRequired()])
 
-    housing = RadioField(
-        "Government intervention is necessary for affordable housing.",
-        choices=LIKERT_CHOICES, validators=[DataRequired()])
-    economy = RadioField(
-        "Job creation is more important than wealth redistribution in California.",
-        choices=LIKERT_CHOICES, validators=[DataRequired()])
-    environment = RadioField(
-        "Environmental protection is worth slowing economic growth in California.",
-        choices=LIKERT_CHOICES, validators=[DataRequired()])
-    immigration = RadioField(
-        "California's resources should primarily serve its citizens, not undocumented immigrants.",
-        choices=LIKERT_CHOICES, validators=[DataRequired()])
-    income_inequality = RadioField(
-        "Reducing income inequality is more important than promoting business growth in California.",
-        choices=LIKERT_CHOICES, validators=[DataRequired()])
-    transportation = RadioField(
-        "Public transportation is more important than private vehicle infrastructure in California.",
-        choices=LIKERT_CHOICES, validators=[DataRequired()])
-    education = RadioField(
-        "Public education funding is more important than tax cuts in California.",
-        choices=LIKERT_CHOICES, validators=[DataRequired()])
-    healthcare = RadioField(
-        "A single-payer healthcare system is preferable to private healthcare in California.",
-        choices=LIKERT_CHOICES, validators=[DataRequired()])
-    public_safety = RadioField(
-        "Community programs are more effective than traditional law enforcement in California.",
-        choices=LIKERT_CHOICES, validators=[DataRequired()])
-    taxation = RadioField(
-        "Progressive taxation is more beneficial than lower taxes for all in California.",
-        choices=LIKERT_CHOICES, validators=[DataRequired()])
+    # Dynamically create the Likert scale fields
+    for likert_choice in VoterInfo.likert_choices:
+        vars()[likert_choice] = RadioField(
+            QUESTION_TEXT[likert_choice],
+            choices=LIKERT_CHOICES,
+            validators=[DataRequired()]
+        )
 
     submit = SubmitField('Submit')
+
+
+@app.route('/skip-intake', methods=['GET'])
+def skip_intake():
+    data = {'zip_code': '94666', 'party_affiliation': 'democrat', 'political_issues': ['healthcare', 'housing'],
+            'housing': '5', 'economy': '1', 'environment': '5', 'immigration': '2', 'income_inequality': '5',
+            'transportation': '4', 'education': '2', 'healthcare': '5', 'public_safety': '2', 'taxation': '5'}
+    voter_info = VoterInfo.from_vals(**data).for_llm()
+    print(f"updating session with {voter_info}")
+    session['voter_info'] = voter_info
+    session.modified = True
+    response = make_response('', 204)
+    response.mimetype = 'application/json'
+    return response
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -151,7 +189,7 @@ def index():
         # remove csrf token and any other unnecessary WTF-form inserted fields
         # just preserve the actual user-inputted data
         form.populate_obj(voter_info)
-        session['form_data'] = json.dumps(voter_info.__dict__)
+        session['voter_info'] = voter_info.for_llm()
 
         session.modified = True
         print(form_data)
@@ -164,10 +202,10 @@ def index():
 def get_data():
     data = request.get_json()
     text = data.get('data')
-    voter_info = VoterInfoDecoder().decode(session.get('form_data'))
+    voter_info = session.get('voter_info')
 
     user_input = str(voter_info) + text
-
+    print(user_input)
     try:
         conversation = ConversationChain(llm=llm, memory=memory)
         output = conversation.predict(input=user_input)
